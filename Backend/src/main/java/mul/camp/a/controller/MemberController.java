5f900970843e8e487e2a05af81e57248d376107c
study-with-HONG <hsilver084@naver.com>
2022-03-21 17:33:05 +0900

    로그인(아이디, 비밀번호 일치), 회원가입(아이디확인, 회원확인)

---
+++ Backend/src/main/java/mul/camp/a/controller/MemberController.java
@@ -0,0 +1,45 @@
+package mul.camp.a.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import mul.camp.a.dto.MemberDto;
+import mul.camp.a.service.MemberService;
+
+@RestController
+public class MemberController {
+	@Autowired
+	MemberService sv;
+	
+	@RequestMapping(value = "/getId", method = {RequestMethod.GET, RequestMethod.POST})
+	public String getId(MemberDto dto) {
+		System.out.println("MemberController getId" + new Date());
+		
+		boolean b = sv.getId(dto);
+		if(b) {
+			return "NO";
+		}
+		return "YES";
+	}
+	@RequestMapping(value = "/addMember", method = {RequestMethod.GET, RequestMethod.POST})
+	public String addMember(MemberDto dto) {
+		System.out.println("MemberController addMember" + new Date());
+		
+		boolean b = sv.addMember(dto);
+		if(b) {
+			return "YES";
+		}
+		return "NO";
+	}
+	@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+	public MemberDto login(MemberDto dto) {
+		System.out.println("MemberController login");
+		
+		MemberDto mem = sv.login(dto);
+		return mem;
+	}	
+}
---
+++ Backend/src/main/java/mul/camp/a/dao/MemberDao.java
@@ -0,0 +1,14 @@
+package mul.camp.a.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import mul.camp.a.dto.MemberDto;
+
+@Mapper
+@Repository
+public interface MemberDao {
+	public int getId(MemberDto dto);	
+	public int addMember(MemberDto dto);
+	public MemberDto login(MemberDto dto);
+}
